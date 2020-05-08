de5ec967dd7b7f631149f08e2511b3e8a5f901cb
Vanessa <vanessatellez45@gmail.com>
2020-05-08 13:20:51 -0400

    ex4 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,16 @@
 def has_negatives(a):
+    nums = {}
+    result = [] #Need to return a list of nums with negs
 
-    """
-    YOUR CODE HERE
-    """
+    #Loop through each number in the list
+    for num in a:
+        #need to check if the absolute value of our number is in our dict..
+        if nums.get(abs(num)):
+            #If if is, we can add it to our result list
+            result.append(abs(num))
+        else:
+            #If not we can add the absoulte value to our dict
+            nums[abs(num)] = num
 
     return result
 
